38539695d36b39be576fc1790e97943b97983b44
MrKikoYT <42118485+MrKikoYT@users.noreply.github.com>
2018-08-05 17:35:00 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,12 +6,7 @@ exports.run = (client, message, args) => {
     .setColor([54, 57, 63])
     .setAuthor("Lista de Comandos", client.user.avatarURL)
     .setDescription("Meu prefix neste servidor: \`" + client.prefix + "\`")
-    .addField("-ping", ":ping_pong: Ping Do Bot!", true)
-    .addField("-bot", ":robot: Informações Do Bot!", true)
-    .addField("-help", ":scroll: Comandos Do Bot!", true)
-    .addField("-meme", ":clown: Imagens Engraçadas!", true)
-    .addField("-svinfo", ":clipboard: Informações Do Servidor!", true)
-    .addField("-thanks", ":heart: Agradecimentos!", true)  
+    .addField("COMANDO", "DESCRIÇÃO DO COMANDO", true) 
     .addField("<:botpng:474617537183744000>", "<VOSSO ICON> Bot Criado Por NOME#ID")
     .setTimestamp();
   
